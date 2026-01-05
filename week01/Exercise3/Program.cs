09b164d10be7f5a3f31f537916dd132333ca130e
Anna Larsen <alarsen9@byupathway.edu>
2026-01-05 10:59:19 -0700

    Finished Exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,61 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string userGuess = "";
+        string keepPlaying = "yes";
+        int guess = 0;
+        int count = 0;
+        int magicNumber = 0;
+
+        Random randomGenerator = new Random();
+
+        Console.WriteLine("Number Guessing Game!");
+        Console.WriteLine();
+        
+        while(keepPlaying == "yes" || keepPlaying == "Yes")
+        {
+            magicNumber = randomGenerator.Next(1, 101);
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                count += 1;         
+                userGuess = Console.ReadLine();
+                guess = int.Parse(userGuess);
+
+                if(guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+
+                else if(guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+
+            } while (guess != magicNumber);
+
+            Console.WriteLine();
+            Console.WriteLine("You guessed it!");
+           
+            if(count == 1)
+            {
+                Console.WriteLine($"It took you {count} guess.");
+            }
+
+            else
+            {
+                Console.WriteLine($"It took you {count} guesses.");
+            }
+            count = 0;
+            
+            Console.WriteLine();
+            Console.Write("Do you want to play again? ");
+            keepPlaying = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Thank you for playing. Goodbye.");
     }
 }
