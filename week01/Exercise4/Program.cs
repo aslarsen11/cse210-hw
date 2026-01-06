e21a65385ec00bf5747608721173bed79975c463
Anna Larsen <alarsen9@byupathway.edu>
2026-01-05 17:26:22 -0700

    Finished Exercise4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,57 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        int userNumber = -1;
+        int largestNum = -99999999;
+        int smallestNum = 99999999;
+        int sum = 0;
+        int count = 0;
+        List<int>numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.Write("Enter number: ");
+            userNumber = int.Parse(Console.ReadLine());
+          
+            if(userNumber != 0)
+            {
+                numbers.Add(userNumber);
+                sum += userNumber;
+                count += 1;
+            }
+
+            if(userNumber > largestNum)
+            {
+                largestNum = userNumber;
+            }
+
+            if(userNumber > 0 && userNumber < smallestNum)
+            {
+                smallestNum = userNumber;
+            }
+
+        } while(userNumber != 0);
+
+        float average = ((float)sum) / count;
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largestNum}");
+        Console.WriteLine($"The smallest positive number is: {smallestNum}");
+
+        // Sort the numbers in the list
+        numbers.Sort();
+
+        Console.WriteLine("The sorted list is:");
+        // Loop to display the sorted list
+        foreach(int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
